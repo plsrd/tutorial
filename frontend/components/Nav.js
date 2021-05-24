1f713c6691d0cfe3a48c549fe7d5b7b474a7c49a
racheallarimer <racheal.larimer@gmail.com>
2021-05-24 11:29:10 -0700

    add nav link to Nav

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Nav.module.css'
 
 const Nav = () => {
   return (
@@ -8,6 +8,14 @@ const Nav = () => {
       <Link href='https://www.sanity.io/'>
         <a><Image src='/../public/sanity.svg' height={30} width={100} /></a>
       </Link>
+      <div>
+        <Link href='https://www.sanity.io/docs' >
+          <a className={styles.navLink}>Docs</a>
+        </Link>
+        <Link href='https://www.sanity.io/exchange'>
+          <a className={styles.navLink}>Exchange</a>
+        </Link>
+      </div>
     </nav>
   )
 
