80821c6fb16046a1b2e4d435175493561d033669
racheallarimer <racheal.larimer@gmail.com>
2021-05-24 12:22:04 -0700

    add heading to Home

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,23 +1,27 @@
-import Head from 'next/head'
 import { sanityClient, urlFor } from '../sanity'
+
+import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+
 import styles from '../styles/Home.module.css'
 
 import Nav from '../components/Nav'
 
-const Home = ({ sections }) => {
-  console.log(sections)
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Sanity 101</title>
+        <title>Sanity Tutorial</title>
         <meta name="description" content="A beginner's guide to Sanity" />
         <link rel="icon" type="image/svg+xml" href="/s.svg" />
       </Head>
       <Nav />
       <main>
-        <p>main</p>
+        <div className={styles.headingContainer}>
+          <h1 className={styles.mainHeading}>New to Sanity?</h1>
+          <p className={styles.subheading}>Learn how to use the powerful unified content plaftorm here</p>
+        </div>
       </main>
       <footer>
         <p>footer</p>
@@ -26,31 +30,31 @@ const Home = ({ sections }) => {
   )
 }
 
-export const getServerSideProps = async () => {
-  const query = `*[_type == 'section']{
-    title, 
-    icon, 
-    slug, 
-    description, 
-    modules[]->{
-      title
-    }
-  }`
-  const sections = await sanityClient.fetch(query)
+// export const getServerSideProps = async () => {
+//   const query = `*[_type == 'section']{
+//     title, 
+//     icon, 
+//     slug, 
+//     description, 
+//     modules[]->{
+//       title
+//     }
+//   }`
+//   const sections = await sanityClient.fetch(query)
 
-  if (! sections) {
-    return {
-      props: {
-        sections: []
-      }
-    }
-  } else {
-    return {
-      props: {
-        sections
-      }
-    }
-  }
-}
+//   if (! sections) {
+//     return {
+//       props: {
+//         sections: []
+//       }
+//     }
+//   } else {
+//     return {
+//       props: {
+//         sections
+//       }
+//     }
+//   }
+// }
 
 export default Home
